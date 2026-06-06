c52bf33a060aef2e7114e854f10dbd8dbc3294a0
Tasin-Hossain <miniofficial51@gmail.com>
2026-06-06 17:38:37 +0600

    feat: Add SidebarLayout and CategoryPage routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { Route, Routes } from 'react-router';
 import RootPage from './pages/RootPage';
+import SidebarLayout from './components/layout/SidebarLayout';
+import CategoryPage from './pages/CategoryPage';
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<RootPage />} />
+        <Route
+          exact
+          path="/:category/:subcategory"
+          element={
+            <SidebarLayout>
+              <CategoryPage />
+            </SidebarLayout>
+          }
+        />
       </Routes>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router';
 import { ThemeProvider } from './components/context/ThemeContext.jsx';
 
+
 createRoot(document.getElementById('root')).render(
+
+
   <ThemeProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </ThemeProvider>
+
 );
