f6031cb650d00a8bb85512eea7cbc70f4016f672
Tasin-Hossain <miniofficial51@gmail.com>
2026-06-07 14:34:46 +0600

    feat: Add FavoritesPage route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router';
 import RootPage from './pages/RootPage';
 import SidebarLayout from './components/layout/SidebarLayout';
 import CategoryPage from './pages/CategoryPage';
+import FavoritesPage from './pages/FavoritesPage';
 
 const App = () => {
   return (
@@ -9,11 +10,18 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<RootPage />} />
         <Route
-          exact
           path="/:category/:subcategory"
           element={
             <SidebarLayout>
               <CategoryPage />
+            </SidebarLayout>
+          }
+        />
+        <Route
+          path="/favorites"
+          element={
+            <SidebarLayout>
+              <FavoritesPage />
             </SidebarLayout>
           }
         />
