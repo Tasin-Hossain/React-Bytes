241dca8e3b3f2dd8df3801d7b9eec0af0bcdae9b
Tasin-Hossain <miniofficial51@gmail.com>
2026-06-07 14:34:58 +0600

    feat: Add COMPONENT_VIDEOS constant for video resources

---
+++ src/constants/ComponentVideos.js
@@ -0,0 +1,7 @@
+
+export const COMPONENT_VIDEOS = {
+  'text-animations/magnetic-text': null,
+  'animations/animated-content':   null,
+  'components/animated-list':      null,
+  'backgrounds/mouserepel-dots':   null,
+};
