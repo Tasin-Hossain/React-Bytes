6882ca429b12747205ff6d4614a0f79b6cf31a0c
Tasin-Hossain <miniofficial51@gmail.com>
2026-06-08 21:53:02 +0600

    feat: Add DemoBuilder component for enhanced layout and functionality

---
+++ src/components/layout/DemoBuilder.jsx
@@ -0,0 +1,63 @@
+import CodeTab from "../shared/code/CodeTab";
+import PreviewTab from "../shared/preview/PreviewTab";
+import TabsLayout from "../shared/TabsLayout";
+import CopyPromptButton from "../ui/Button/CopyPromptButton";
+import FavoriteButton from "../ui/Button/FavoriteButton";
+
+export default function DemoBuilder({
+  title,
+  favKey,
+  prompt,
+  PreviewComponent,
+  customize,
+  dependencies,
+  propsTable,
+  footer,
+
+  // CodeTab props
+  pkgCmds,
+  shadcnCmds,
+  usageCode,
+  codeVariants,
+  cssCode,
+  CodeBlock,
+}) {
+  return (
+    <div className="md:max-w-4xl lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl mx-auto text-(--text-primary) relative">
+      <div className="flex items-center justify-between gap-4 mb-5">
+        <h1 className="title mb-0">{title}</h1>
+
+        <div className="flex items-center gap-2">
+          <FavoriteButton favKey={favKey} />
+          <CopyPromptButton text={prompt} />
+        </div>
+      </div>
+
+      <TabsLayout
+        preview={
+          <>
+            <PreviewTab>
+              {PreviewComponent}
+            </PreviewTab>
+
+            {customize}
+            {propsTable}
+            {dependencies}
+          </>
+        }
+        code={
+          <CodeTab
+            pkgCmds={pkgCmds}
+            shadcnCmds={shadcnCmds}
+            usageCode={usageCode}
+            codeVariants={codeVariants}
+            cssCode={cssCode}
+            CodeBlock={CodeBlock}
+          />
+        }
+      />
+
+      {footer}
+    </div>
+  );
+}
