b8cc1d157f9abadf230ca1fa199f2a909d2a88cf
Tasin-Hossain <miniofficial51@gmail.com>
2026-06-08 21:52:39 +0600

    feat: Add onclick prop to Button component for enhanced interactivity

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -4,12 +4,14 @@ const Button = ({
   text = 'Community',
   icon,
   className = '',
+  onclick,
   ...props
 }) => {
   return (
     <button
       type="button"
       className={`${className} relative inline-flex cursor-pointer items-center justify-center overflow-hidden rounded-md border-none bg-[linear-gradient(0deg,#7a5af8,#7a5af8),radial-gradient(65.28%_65.28%_at_50%_100%,rgba(223,113,255,0.8)_0%,rgba(223,113,255,0)_100%)] px-3 py-1.5 outline-none transition-all duration-300 active:scale-95 group button-custom`}
+      onClick={onclick}
       {...props}
     >
       <div className="absolute inset-0 z-1 overflow-hidden pointer-events-none">
