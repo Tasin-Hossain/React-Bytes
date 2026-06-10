f7a508a7b779ee052a446c61906d324e02cebf9e
Tasin-Hossain <miniofficial51@gmail.com>
2026-06-10 13:11:19 +0600

    feat: Wrap application in HelmetProvider for improved document head management

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,14 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router';
 import { ThemeProvider } from './components/context/ThemeContext.jsx';
-
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
-
-
-  <ThemeProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>
-
+  <HelmetProvider>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </HelmetProvider>
 );
