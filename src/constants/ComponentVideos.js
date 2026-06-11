b29ae967af67cb9538f3342b95ee86e995393e99
Tasin-Hossain <miniofficial51@gmail.com>
2026-06-11 12:47:53 +0600

    feat: Add curtainText video to COMPONENT_VIDEOS for text animations

--- src/constants/ComponentVideos.js
+++ src/constants/ComponentVideos.js
@@ -1,7 +1,9 @@
 import magneticText from '../assets/videos/TextAnimations/magnetic-text.mp4';
+import curtainText from '../assets/videos/TextAnimations/curtain-text.mp4';
 
 export const COMPONENT_VIDEOS = {
   'text-animations/magnetic-text': magneticText,
+  'text-animations/curtain-text': curtainText,
   'animations/animated-content':   null,
   'components/animated-list':      null,
   'backgrounds/mouserepel-dots':   null,
