496f9fdeb34ca7a0e193dd3339a9df7b5286486c
Tasin-Hossain <miniofficial51@gmail.com>
2026-06-13 11:56:46 +0600

    feat: add cursor-trail video to ComponentVideos for text animations

--- src/constants/ComponentVideos.js
+++ src/constants/ComponentVideos.js
@@ -1,9 +1,11 @@
 import magneticText from '../assets/videos/TextAnimations/magnetic-text.mp4';
 import curtainText from '../assets/videos/TextAnimations/curtain-text.mp4';
+import cursorTrail from '../assets/videos/TextAnimations/cursor-trail.mp4';
 
 export const COMPONENT_VIDEOS = {
   'text-animations/magnetic-text': magneticText,
   'text-animations/curtain-text': curtainText,
+  'text-animations/cursor-trail': cursorTrail,
   'animations/animated-content':   null,
   'components/animated-list':      null,
   'backgrounds/mouserepel-dots':   null,
