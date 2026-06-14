c63e8fccbe7f15e39b1869b6d5c843e892fd2060
Tasin-Hossain <miniofficial51@gmail.com>
2026-06-14 15:20:25 +0600

    feat: add MouseRepelGridDemo to componentMap for backgrounds

--- src/constants/componentMap.js
+++ src/constants/componentMap.js
@@ -14,6 +14,7 @@ const componentMap = {
 
 
   'backgrounds/mouserepel-dots': () => import('../demo/Backgrounds/MouseRepelDotsDemo.jsx'),
+  'backgrounds/mouserepel-grid': () => import('../demo/Backgrounds/MouseRepelGridDemo.jsx'),
 
 
 };
