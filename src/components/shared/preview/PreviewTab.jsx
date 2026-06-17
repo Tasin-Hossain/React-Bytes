83a087820fcae2b86b8d0af244e67a0253d0e8f4
Tasin-Hossain <miniofficial51@gmail.com>
2026-06-17 15:32:24 +0600

    refactor: remove unused Replay functionality and associated component from PreviewTab

--- src/components/shared/preview/PreviewTab.jsx
+++ src/components/shared/preview/PreviewTab.jsx
@@ -1,24 +1,23 @@
 import useComponentProps from '../../../hooks/useComponentProps';
-import { FaArrowRotateLeft } from 'react-icons/fa6';
+
 import DemoOverlay from './DemoOverlay';
 
 const PreviewTab = ({
   className,
   children,
   childrenClassname,
-  showReplay = true,
   minHeight = 'min-h-120!',
   demoContent = false,
   onToggleDemoContent
 }) => {
-  const { replay, animKey } = useComponentProps();
+  const {  animKey } = useComponentProps();
 
   return (
     <div
       className={`relative rounded-md border border-(--border-secondary) bg-(--bg-white)/3 z-10
         flex items-center justify-center ${minHeight} mb-6 overflow-hidden ${className}`}
     >
-      {showReplay && <RefreshButton onClick={replay} />}
+
 
       {/* childrenClassname for backgrounds  */}
       <div key={animKey} className={` ${childrenClassname}  `}>
@@ -69,16 +68,6 @@ const PreviewTab = ({
   );
 };
 
-export const RefreshButton = ({ onClick }) => (
-  <button
-    onClick={onClick}
-    title="Replay"
-    className="absolute top-3 right-3 w-9 h-9 flex items-center justify-center
-      rounded-md border border-(--border-button) bg-(--bg-button) 
-      text-(--text-muted) hover:text-(--text-primary) transition-all duration-150 cursor-pointer"
-  >
-    <FaArrowRotateLeft />
-  </button>
-);
+
 
 export default PreviewTab;
