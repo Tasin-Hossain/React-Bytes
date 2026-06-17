1bfe86b2064dc0d678d830a70ba53b34c4fee931
Tasin-Hossain <miniofficial51@gmail.com>
2026-06-17 15:32:08 +0600

    fix: update button text and class for CopyPromptButton component

--- src/components/ui/Button/CopyPromptButton.jsx
+++ src/components/ui/Button/CopyPromptButton.jsx
@@ -35,12 +35,12 @@ const CopyPromptButton = ({ text = '' }) => {
   return (
     <button
       onClick={handleCopy}
-      className="h-9 flex items-center gap-1.5 text-sm px-3 py-2 rounded-md
+      className="h-9 flex items-center gap-1.5 text-sm px-3 py-2 rounded-md truncate
         border border-(--border-button) bg-(--bg-button) text-(--text-muted)
         hover:text-(--text-primary) transition-all duration-150 cursor-pointer"
     >
       {copied ? <FaCheck size={18} /> : <LuCopy size={18} />}
-      {copied ? 'Copied' : 'Copy Prompt'}
+      {copied ? 'Prompt' : 'Prompt'}
     </button>
   );
 };
