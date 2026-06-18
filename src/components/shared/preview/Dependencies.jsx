ddb48c13dffd21efc3b7ed3f2240b9222ac79853
Tasin-Hossain <miniofficial51@gmail.com>
2026-06-18 13:21:11 +0600

    feat: enhance Dependencies component to handle default empty array and display dependency versions

--- src/components/shared/preview/Dependencies.jsx
+++ src/components/shared/preview/Dependencies.jsx
@@ -1,16 +1,20 @@
-const Dependencies = ({dependencies}) => {
+const Dependencies = ({ dependencies = [] }) => {
   return (
     <section>
-      <h2 className="title-two mb-4">Dependencies</h2>
-      <div className="flex gap-2">
-        {dependencies.map(dep => (
-          <span
-            key={dep}
+
+        <h2 className="title-two mb-4">Dependencies</h2>
+      <div className="flex flex-wrap gap-2">
+        {dependencies.map((dep) => (
+          <>
+            <span
+            key={dep.name}
             className="text-sm px-3 py-1 rounded-md bg-(--bg-button)
-                    border border-(--border-button) text-(--text-muted)"
+                       border border-(--border-button)
+                       text-(--text-muted)"
           >
-            {dep}
+            {dep.name} <span className="opacity-70">{dep.version}</span>
           </span>
+          </>
         ))}
       </div>
     </section>
