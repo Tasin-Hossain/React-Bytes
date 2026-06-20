b973fe7ddb210b8a0b291cc681489ef44d9bd7b2
Tasin-Hossain <miniofficial51@gmail.com>
2026-06-20 12:33:32 +0600

    fix: improve Dependencies component rendering and styling

--- src/components/shared/preview/Dependencies.jsx
+++ src/components/shared/preview/Dependencies.jsx
@@ -1,20 +1,19 @@
 const Dependencies = ({ dependencies = [] }) => {
+  if (dependencies.length === 0) return null;
+
   return (
     <section>
-
-        <h2 className="title-two mb-4">Dependencies</h2>
+      <h2 className="title-two mb-4">Dependencies</h2>
       <div className="flex flex-wrap gap-2">
         {dependencies.map((dep) => (
-          <>
-            <span
+          <span
             key={dep.name}
             className="text-sm px-3 py-1 rounded-md bg-(--bg-button)
                        border border-(--border-button)
                        text-(--text-muted)"
           >
-            {dep.name} <span className="opacity-70">{dep.version}</span>
+            {dep.name} <span className="opacity-70 text-xs">{dep.version}</span>
           </span>
-          </>
         ))}
       </div>
     </section>
